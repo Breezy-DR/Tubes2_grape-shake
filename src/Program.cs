3a4a3ef9b3e206a50da95160c6bbce93cd4c0f22
BreezyDR <13521048@std.stei.itb.ac.id>
2023-03-20 15:38:18 +0700

    update main

--- src/Program.cs
+++ src/Program.cs
@@ -5,35 +5,19 @@ namespace Maze {
     class Program {
         static void Main(string[] args) {
             try {
+                string filename;
+                Console.Write("Masukkan nama file: ");
+                filename = Console.ReadLine();
                 Utils ut = new Utils();
-                string[][] jag = ut.ReadFile("test1.txt");
-                List<int> LineElementCount = new List<int>();
-                for (int x = 0; x < jag.Length; x++) {
-                    int countelement = 0;
-                    for (int y = 0; y < jag[x].Length; y++) {
-                        Console.Write(jag[x][y] + " ");
-                        countelement += 1;
-                    }
-                    Console.WriteLine();
-                    LineElementCount.Add(countelement);
-                }
-                int i;
-                for (i = 1; i < LineElementCount.Count; i++) {
-                    if (LineElementCount[0] == LineElementCount[i]) {
-                        continue;
-                    } else {
-                        break;
-                    }
-                }
-                if (i != LineElementCount.Count) {
-                    throw new ArgumentOutOfRangeException("All lines have to have the same number of elements");
-                }
-                for (int x = 0; x < jag.Length; x++) {
-                    for (int y = 0; y < jag[x].Length; y++) {
-                        if (jag[x][y] != "K" && jag[x][y] != "R" && jag[x][y] != "X" && jag[x][y] != "T") {
-                            throw new ArgumentOutOfRangeException("File symbols can only be X, R, K, and T");
-                        }
-                    }
+                string[][] jag = ut.ReadFile(filename);
+                ut.printMatrix(jag);
+                if (!ut.isLineHaveEqualElement(jag)) {
+                    Console.WriteLine("All lines have to have the same number of elements");
+                } else if (!ut.isSymbolValid(jag)) {
+                    Console.WriteLine("File symbols can only be X, R, K, and T");
+                } else {
+                    Console.WriteLine("File is valid.");
+                    // the rest of the BFS, DFS code
                 }
             } catch (Exception e) {
                 Console.WriteLine(e.Message);
--- src/Utils.cs
+++ src/Utils.cs
@@ -9,5 +9,49 @@ namespace Maze {
             .Select(l => l.Split(' ').ToArray()).ToArray();
             return list;
         }
+
+        public bool isSymbolValid(string[][] jag) {
+            for (int x = 0; x < jag.Length; x++) {
+                    for (int y = 0; y < jag[x].Length; y++) {
+                        if (jag[x][y] != "K" && jag[x][y] != "R" && jag[x][y] != "X" && jag[x][y] != "T") {
+                            return false;
+                        }
+                    }
+            }
+            return true;
+        }
+
+        public void printMatrix(string[][] jag) {
+            for (int x = 0; x < jag.Length; x++) {
+                    for (int y = 0; y < jag[x].Length; y++) {
+                        Console.Write(jag[x][y] + " ");
+                    }
+                    Console.WriteLine(); 
+                }
+        }
+
+        public bool isLineHaveEqualElement(string[][] jag) {
+            List<int> LineElementCount = new List<int>();
+            for (int x = 0; x < jag.Length; x++) {
+                    int countelement = 0;
+                    for (int y = 0; y < jag[x].Length; y++) {
+                        countelement += 1;
+                    }
+                    LineElementCount.Add(countelement);
+            }
+            int i;
+                for (i = 1; i < LineElementCount.Count; i++) {
+                    if (LineElementCount[0] == LineElementCount[i]) {
+                        continue;
+                    } else {
+                        break;
+                    }
+            }
+            if (i != LineElementCount.Count) {
+                    return false;
+            } else {
+                return true;
+            }
+        }
     }
 }
